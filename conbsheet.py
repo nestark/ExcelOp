def9c649bf59dbf81b3c3f3b1a763186c6223dd9
nestark <nestark@users.noreply.github.com>
2019-02-18 19:01:10 +0800

    reformation the worksheet

--- conbsheet.py
+++ conbsheet.py
@@ -11,17 +11,22 @@ for worksheet in a:
     ws1=wb1.active
     time_range = ws1['B4:B16']
     value_range = ws1['E4:E16']
-    for i in range(n, n+13):
-        ws2[i][0].value=time_range[i-n][0].value
-        ws2.cell(row=i,column=2).value=value_range[i-n][0].value
-        ws2.cell(row=i, column=3).value=ws1.title[4:7]
-        ws2.cell(row=i,column=4).value=ws1.title[12:14]
-    n+=13
+
+    if ws1.title[12:14]=='温度':
+        for i in range(n, n + 13):
+            ws2.cell(row=i,column=1).value = time_range[i-n][0].value
+            ws2.cell(row=i, column=2).value = ws1.title[4:7]
+            ws2.cell(row=i,column=3).value=value_range[i-n][0].value
+    else:
+        for i in range(n, n + 13):
+            ws2.cell(row=i, column=4).value = value_range[i-n][0].value
+        n+=13
+
 ws2.insert_rows(0)
 ws2['A1']='日期'
-ws2['B1']='数值'
-ws2['C1']='机房'
-ws2['D1']='类型'
+ws2['B1']='机房'
+ws2['C1']='温度'
+ws2['D1']='湿度'
 
 wb2.save('summary.xlsx')
 print('数据汇总已保存在summary.xlsx')
